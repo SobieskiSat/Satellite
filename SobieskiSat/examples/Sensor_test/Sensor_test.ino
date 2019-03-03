ec6c96fa08d83dc311c86ed642ddf1f8e0e83007
RustleofCicada <39823912+RustleofCicada@users.noreply.github.com>
2019-03-03 12:35:17 +0100

    Add files via upload

--- SobieskiSat/examples/Sensor_test/Sensor_test.ino
+++ SobieskiSat/examples/Sensor_test/Sensor_test.ino
@@ -6,9 +6,9 @@ BMP280 sensor; // possible: BMP280, DHT22, SPS30, GPS, MPU
 
 void setup()
 {
-  SerialUSB.begin(112500); 
+  SerialUSB.begin(115200); 
   if (sensor.begin()) SerialUSB.println("Initialization complete.");
-  else SerialUSB.println("Failed to initialize. (in case of GPS wait for Fix)"); 
+  else SerialUSB.println("Failed to initialize."); 
 }
 
 void loop()
