b1d617cc14d45eed33b28b6d869d54021648f2b5
RustleofCicada <39823912+RustleofCicada@users.noreply.github.com>
2019-03-03 12:19:37 +0100

    Add files via upload

---
+++ SobieskiSat/examples/Sensor_test/Sensor_test.ino
@@ -0,0 +1,19 @@
+#include <SobieskiSat.h>
+
+using namespace SobieskiSat;
+
+BMP280 sensor; // possible: BMP280, DHT22, SPS, GPS, MPU
+
+void setup()
+{
+  SerialUSB.begin(112500); 
+  sensor.begin();
+}
+
+void loop()
+{
+  if (sensor.update())
+  {
+	  SerialUSB.println(sensor.listReadings());
+  }
+}
