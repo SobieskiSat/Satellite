120120b262ee0224cf04bbbcf4a0e44a002fed0c
RustleofCicada <39823912+RustleofCicada@users.noreply.github.com>
2019-03-22 09:48:31 +0000

    Update Antenna.ino

--- examples/Antenna/Antenna.ino
+++ examples/Antenna/Antenna.ino
@@ -113,7 +113,7 @@ if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64 ZMI
   radio.begin();
   
   compressor.clear();
-  compressor.format = "0_26_Latitude_49.0000000_54.5000000_2_7 26_53_Longitude_14.0699997_24.0900002_2_7 53_67_Altitude_0.0_1000.0_4_1 67_90_Pressure_600.0000_1200.0000_4_4 90_103_Temperature_-20.00_50.00_2_2 103_113_PM25_0.0_100.0_3_1 113_123_PM100_0.0_100.0_3_1 123_128_PackNo_0_32_2_0X";
+  compressor.format = "0_26_Latitude_49.0000000_54.5000000_2_7 26_53_Longitude_14.0699997_24.0900002_2_7 53_67_Altitude_0.0_1000.0_4_1 67_90_Pressure_600.0000_1200.0000_4_4 90_103_Temperature_-20.00_50.00_2_2 103_113_PM25_0.0_100.0_3_1 113_123_PM100_0.0_100.0_3_1 123_128_PackNo_0_32_2_0 X";
 
 //OLED
   display.clearDisplay();
